730f32cac00c28344d8eec0c141bda22d816b7b4
José Quinteiro <a23485@alunos.ipca.pt>
2022-11-29 10:23:57 +0000

    Adicionar arquivos de projeto.

---
+++ ProjetoPoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
