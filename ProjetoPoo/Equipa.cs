b30c7b12b061836a5f193828ad369ecdd06ccd79
José Quinteiro <a23485@alunos.ipca.pt>
2022-12-06 13:19:35 +0000

    Equipa

---
+++ ProjetoPoo/Equipa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoPoo
+{
+    class Equipa
+    {
+        public int id;
+        public string nome;
+
+        public Equipa(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+    }
+}
